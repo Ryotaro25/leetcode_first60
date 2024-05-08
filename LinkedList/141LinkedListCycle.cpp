e81d1ebeb38bbff54e3bbfe3ae3b22e07794bf2b
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-05-09 08:13:57 +0900

    step 2

--- LinkedList/141LinkedListCycle.cpp
+++ LinkedList/141LinkedListCycle.cpp
@@ -12,13 +12,15 @@ public:
         ListNode* fast = head;
         ListNode* slow = head;
 
-        while(fast != nullptr && fast->next != nullptr) {
+        while (fast != nullptr && fast->next != nullptr) {
             fast = fast->next->next;
             slow = slow->next;
-            if(fast == slow) {
+
+            if (fast == slow) {
                 return true;
             }
         }
+
         return false;
     }
 };
