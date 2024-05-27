e69c0549220f040da87162b8704aca9d7a845411
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-05-28 08:27:27 +0900

    modify

--- 20.ValidParentheses/step4.cpp
+++ 20.ValidParentheses/step4.cpp
@@ -5,17 +5,20 @@ class Solution {
 public:
     bool isValid(string s) {
         std::stack<char> open_brackets;
-        std::map<char, char> brackets_pairs;
-        brackets_pairs['('] = ')';
-        brackets_pairs['['] = ']';
-        brackets_pairs['{'] = '}';
+        std::map<char, char> brackets_pairs = {
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'},
+        };
 
         for (int i = 0; i < s.length(); i++) {
             if (brackets_pairs.contains(s[i])) {
                 open_brackets.push(s[i]);
                 continue;
-            } else if (open_brackets.empty() ||
-                       brackets_pairs[open_brackets.top()] != s[i]) {
+            }
+
+            if (open_brackets.empty() ||
+                brackets_pairs[open_brackets.top()] != s[i]) {
                 return false;
             }
             open_brackets.pop();
