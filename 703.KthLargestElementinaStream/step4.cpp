454f938a90e2a67f4a05ba88c5373c05b2486c74
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-06-03 00:59:32 +0900

    modify

--- 703.KthLargestElementinaStream/step4.cpp
+++ 703.KthLargestElementinaStream/step4.cpp
@@ -1,17 +1,17 @@
 class KthLargest {
 public:
-    map<int, int>largest_numbers;
+    priority_queue<int, vector<int>, greater<int>>largest_numbers;
     int kth_number;
     KthLargest(int k, vector<int>& nums) {
         kth_number = k;
-        for (int i : nums) {
-            add(i);
+        for (int num : nums) {
+            add(num);
         }
     }
     
     int add(int val) {
         if (kth_number > largest_numbers.size()) {
-            largest_numbers[val]++;
+            largest_numbers.push(val);
         } else {
             if (val > largest_numbers.top()) {
                 largest_numbers.pop();
