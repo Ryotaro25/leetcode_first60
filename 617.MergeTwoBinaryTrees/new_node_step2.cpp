cd017d49a5d95ff525e3a8cc50d9abfd2f7478e2
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-08-09 17:00:46 +0900

    modifyr member names

--- 617.MergeTwoBinaryTrees/new_node_step2.cpp
+++ 617.MergeTwoBinaryTrees/new_node_step2.cpp
@@ -17,29 +17,29 @@ public:
     search_nodes.emplace(new_root, node1, node2);
 
     while (!search_nodes.empty()) {
-      auto [current_node, first_node, second_node] = search_nodes.front();
+      auto [current_node, left_child, right_child] = search_nodes.front();
       search_nodes.pop();
-      if (!first_node && !second_node) {
+      if (!left_child && !right_child) {
         return nullptr;
       }
 
       int current_val = 0;
-      if (first_node) {
-        current_val += first_node->val;
+      if (left_child) {
+        current_val += left_child->val;
       }
-      if (second_node) {
-        current_val += second_node->val;
+      if (right_child) {
+        current_val += right_child->val;
       }
       current_node->val = current_val;
 
       // 左の子ノードを処理
       TreeNode* left_first = nullptr;
-      if (first_node && first_node->left) {
-        left_first = first_node->left;
+      if (left_child && left_child->left) {
+        left_first = left_child->left;
       }
       TreeNode* left_second = nullptr;
-      if (second_node && second_node->left) {
-        left_second = second_node->left;
+      if (right_child && right_child->left) {
+        left_second = right_child->left;
       }
       if (left_first || left_second) {
         current_node->left = new TreeNode();
@@ -48,12 +48,12 @@ public:
 
       // 右の子ノードを処理
       TreeNode* right_first = nullptr;
-      if (first_node && first_node->right) {
-        right_first = first_node->right;
+      if (left_child && left_child->right) {
+        right_first = left_child->right;
       }
       TreeNode* right_second = nullptr;
-      if (second_node && second_node->right) {
-        right_second = second_node->right;
+      if (right_child && right_child->right) {
+        right_second = right_child->right;
       }
       if (right_first || right_second) {
         current_node->right = new TreeNode();
@@ -66,8 +66,8 @@ public:
 
 private:
   struct Nodes {
-    TreeNode* current_node;
-    TreeNode* first_node;
-    TreeNode* second_node;
+    TreeNode* node;
+    TreeNode* left_child;
+    TreeNode* right_child;
   };
 };
