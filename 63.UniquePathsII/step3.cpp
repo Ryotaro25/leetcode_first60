7fd70ac4a32dd68d892796f1c6f0dd212a24c0dc
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-11-09 08:26:32 +0900

    finish

---
+++ 63.UniquePathsII/step3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int row_size = obstacleGrid.size();
+    int column_size = obstacleGrid[0].size();
+
+    vector<vector<int>> num_paths(row_size, vector<int>(column_size));
+    for (int row = 0; row < row_size; row++) {
+      for (int col = 0; col < column_size; col++) {
+        if (obstacleGrid[row][col] == OBSTACLE) {
+          num_paths[row][col] = 0;
+          continue;
+        }
+        if (row == 0 && col == 0) {
+          num_paths[row][col] = 1;
+          continue;
+        }
+        if (row == 0) {
+          num_paths[row][col] = num_paths[row][col - 1];
+          continue;
+        }
+        if (col == 0) {
+          num_paths[row][col] = num_paths[row - 1][col];
+          continue;
+        }
+        num_paths[row][col] = num_paths[row - 1][col] + num_paths[row][col - 1];
+      }
+    }
+
+    return num_paths[row_size - 1][column_size - 1];
+  }
+
+private:
+  static constexpr int OBSTACLE = 1;
+};
