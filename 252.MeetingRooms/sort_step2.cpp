80fe28d8460d57014cc8dba2fdcd56eac6f177cb
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2025-03-23 09:57:36 +0900

    add sort step 2 mode name

--- 252.MeetingRooms/sort_step2.cpp
+++ 252.MeetingRooms/sort_step2.cpp
@@ -5,12 +5,12 @@ class Solution {
         return true;
       }
       sort(intervals.begin(), intervals.end());
-      int prev_end_ime = intervals[0][1];
+      int prev_end_time = intervals[0][1];
       for (int i = 1; i < intervals.size(); i++) {
-        if (prev_end_ime > intervals[i][0]) {
+        if (prev_end_time > intervals[i][0]) {
           return false;
         }
-        prev_end_ime = intervals[i][1];
+        prev_end_time = intervals[i][1];
       }
       
       return true;
