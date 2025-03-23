ab25137918679ce6bdc41fcdd71052d17e0c2a65
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2025-03-23 09:56:40 +0900

    add sort step 2

---
+++ 252.MeetingRooms/sort_step2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+      if (intervals.size() == 0) {
+        return true;
+      }
+      sort(intervals.begin(), intervals.end());
+      int prev_end_ime = intervals[0][1];
+      for (int i = 1; i < intervals.size(); i++) {
+        if (prev_end_ime > intervals[i][0]) {
+          return false;
+        }
+        prev_end_ime = intervals[i][1];
+      }
+      
+      return true;
+    }
+  };
